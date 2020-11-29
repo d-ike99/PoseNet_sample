999d0baaed6ffefc00284d2a82a2087e977793fb
Kazuhiro Ikeda <avicenxz6310@gmail.com>
2020-11-29 11:10:51 +0900

    Initial Commit

---
+++ testCoreML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testCoreML
+//
+//  Created by 池田和浩 on 2020/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
