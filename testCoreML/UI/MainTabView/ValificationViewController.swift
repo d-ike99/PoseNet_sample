921d6826b53cfd2f9c521ae1d707ea39fd60f735
池田和浩 <avicenxz6310@gmail.com>
2021-04-16 14:22:09 +0900

    #9 対応

--- testCoreML/UI/MainTabView/ValificationViewController.swift
+++ testCoreML/UI/MainTabView/ValificationViewController.swift
@@ -14,7 +14,14 @@ class ValificationViewController: UIViewController {
     var imagePickerController: UIImagePickerController!
     var videoURL: URL?
     
-    var imageView: UIImageView!
+    // 動画表示関連の部品
+    var playerItem: AVPlayerItem!
+    var avplayer: AVPlayer!
+    var playerLayer: AVPlayerLayer!
+
+    // 表示部品
+    var playerView: PlayerView!
+    var link: CADisplayLink!
     
     override func viewDidLoad() {
         
@@ -35,18 +42,38 @@ class ValificationViewController: UIViewController {
         playButton.frame = CGRect(x: 10, y: 100, width: 60, height: 30)
         playButton.setTitle("再生", for: .normal)
         playButton.setTitleColor(.red, for: .normal)
-        playButton.addTarget(self, action: #selector(playMovie), for: UIControl.Event.touchUpInside)
-
         
         // 動画表示1
-        self.imageView = UIImageView()
-        imageView.frame = CGRect(x: self.view.frame.width / 4, y: 30, width: self.view.frame.width / 4 * 3, height: self.view.frame.height / 3)
-        imageView.backgroundColor = .systemGray
+        let playerFrame = CGRect(x: self.view.frame.width / 4, y: 30, width: self.view.frame.width / 4 * 3, height: self.view.frame.height / 3)
+        self.playerView = PlayerView(frame: playerFrame)
+        playerView.backgroundColor = .systemGray
+        playerView.delegate = self
         
         self.view.backgroundColor = .systemGray5
         self.view.addSubview(testButton)
         self.view.addSubview(playButton)
-        self.view.addSubview(imageView)
+        self.view.addSubview(playerView)
+    }
+    
+    //画面消える時にremove
+    deinit{
+        playerItem.removeObserver(self, forKeyPath: "loadedTimeRanges")
+        playerItem.removeObserver(self, forKeyPath: "status")
+    }
+
+   //監視イベント
+   //Unknown 、ReadyToPlay 、 Failed状態があり、readyToPlayの際のみ再生
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard let playerItem = object as? AVPlayerItem else { return }
+            if keyPath == "loadedTimeRanges"{
+                // TODO
+            }else if keyPath == "status"{
+                if playerItem.status == AVPlayerItem.Status.readyToPlay{
+                    self.avplayer.play()
+                }else{
+                    print("error")
+                }
+            }
     }
     
     // 日付ボタン押下処理
@@ -54,7 +81,6 @@ class ValificationViewController: UIViewController {
         print("UIBarButtonItem。カメラロールから動画を選択")
         imagePickerController.sourceType = .photoLibrary
         imagePickerController.delegate = self
-        //imagePickerController.mediaTypes = ["public.image", "public.movie"]
         //動画だけ
         imagePickerController.mediaTypes = ["public.movie"]
         //画像だけ
@@ -62,36 +88,42 @@ class ValificationViewController: UIViewController {
         present(imagePickerController, animated: true, completion: nil)
     }
     
-    @objc func playMovie(_ sender: UIButton) {
+    // シークバー関連の更新
+    @objc func update(){
+        // 動画の再生位置に従い、シークバーを更新する
+        let currentTime = CMTimeGetSeconds(self.avplayer.currentTime())
         
-        print("videoURL: ", videoURL as? Any)
+//        print("playerItem.duration.value: ", playerItem.duration.value)
+//        print("playerItem.duration.value: ", playerItem.duration.timescale)
         
-        if let videoURL = videoURL{
-            let playerItem = AVPlayerItem(url: videoURL)
-            
-            let player = AVPlayer(playerItem: playerItem)
-            
-            let playerViewController = AVPlayerViewController()
-            playerViewController.player = player
-            present(playerViewController, animated: true){
-                print("動画再生")
-                playerViewController.player!.play()
-            }
+        let totalTime   = TimeInterval(playerItem.duration.value) / TimeInterval(playerItem.duration.timescale)
+        let timeStr = "\(formatPlayTime(secounds: currentTime))/\(formatPlayTime(secounds: totalTime))"
+        playerView.timeLabel.text = timeStr
+        
+        // スライダー更新
+        if !self.playerView.sliding {
+            // 播放进度
+            self.playerView.slider.value = Float(currentTime/totalTime)
         }
     }
 }
 
+// 写真アプリからのdelegate
 extension ValificationViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // URL取得
+        let mediaURL = info[.mediaURL] as? URL
         videoURL = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerReferenceURL")] as? URL
-        
-        
         print("videoURL: ", videoURL!)
+        print("mediaURL: ", mediaURL)
+        
+        // player設定
+        self.settingVideoPlayer(url: mediaURL!)
         
         // サムネイル表示
-        //imageView.image = previewImageFromVideo(videoURL!)!
-        imageView.contentMode = .scaleAspectFit
+//        playerLayer.image = previewImageFromVideo(videoURL!)!
+//        imageView.contentMode = .scaleAspectFit
         imagePickerController.dismiss(animated: true, completion: nil)
     }
     
@@ -99,7 +131,7 @@ extension ValificationViewController: UIImagePickerControllerDelegate, UINavigat
     private func previewImageFromVideo(_ url:URL) -> UIImage? {
 
         print("動画からサムネイルを生成する")
-        let asset = AVAsset(url:url)
+        let asset = AVAsset(url: url)
         let imageGenerator = AVAssetImageGenerator(asset:asset)
         imageGenerator.appliesPreferredTrackTransform = true
         
@@ -113,4 +145,70 @@ extension ValificationViewController: UIImagePickerControllerDelegate, UINavigat
             return nil
         }
     }
+    
+    private func settingVideoPlayer(url: URL){
+        let asset = AVAsset(url:url)
+        playerItem = AVPlayerItem(asset: asset) //誰を再生するか決める
+
+        var time = asset.duration
+        print("time: ", time)
+        
+        // 状態監視
+        playerItem.addObserver(self, forKeyPath: "loadedTimeRanges", options: NSKeyValueObservingOptions.new, context: nil)
+        playerItem.addObserver(self, forKeyPath: "status", options: NSKeyValueObservingOptions.new, context: nil)
+        
+        // AVPlayer・AVPlayerLayerインスタンス生成
+        self.avplayer = AVPlayer(playerItem: playerItem)
+        playerLayer = AVPlayerLayer(player: avplayer)
+        
+        // 表示モードの設定
+        playerLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+        playerLayer.contentsScale = UIScreen.main.scale
+        
+        // 表示設定
+        self.playerView.playerLayer = self.playerLayer
+        self.playerView.layer.insertSublayer(playerLayer, at: 0)
+        
+        // 定期的実行（シークバーを更新し続ける？？）
+        self.link = CADisplayLink(target: self, selector: #selector(update))
+        self.link.add( to: RunLoop.main, forMode: RunLoop.Mode.default)
+    }
+}
+
+extension ValificationViewController {
+    private func formatPlayTime(secounds:TimeInterval)->String{
+        if secounds.isNaN{
+            return "00:00"
+        }
+        let Min = Int(secounds / 60)
+        let Sec = Int(secounds.truncatingRemainder(dividingBy: 60))
+        return String(format: "%02d:%02d", Min, Sec)
+    }
+}
+
+
+extension ValificationViewController: TestPlayerViewDelegate {
+    // スライダーのタッチリリースdelegate
+    func testPlayer(playerView: PlayerView, sliderTouchUpOut slider: UISlider) {
+        let duration = slider.value * Float(CMTimeGetSeconds(self.avplayer.currentItem!.duration))
+        let seekTime = CMTimeMake(value: Int64(duration), timescale: 1)
+        // 位置を特定
+        self.avplayer.seek(to: seekTime, completionHandler: { (b) in
+            // sliding状態を戻す
+            playerView.sliding = false
+        })
+    }
+    
+    func testPlayer(playerView: PlayerView, playAndPause playBtn: UIButton) {
+        if !playerView.playing{
+            self.avplayer.pause()
+        }else{
+            print("status: ", avplayer.status as Any)
+            if self.avplayer.status == AVPlayer.Status.readyToPlay {
+                self.avplayer.play()
+            }
+        }
+    }
+    
+    
 }
---
+++ testCoreML/UI/MainTabView/VarificationView/UI/PlayerView.swift
@@ -0,0 +1,93 @@
+//
+//  PlayerView.swift
+//  testCoreML
+//
+//  Created by 池田和浩 on 2021/04/15.
+//
+
+import UIKit
+import AVFoundation
+
+protocol TestPlayerViewDelegate: NSObjectProtocol {
+    func testPlayer(playerView: PlayerView, sliderTouchUpOut slider: UISlider)
+    func testPlayer(playerView: PlayerView, playAndPause playBtn: UIButton)
+}
+
+class PlayerView: UIView {
+    
+    // delegate
+    weak var delegate: TestPlayerViewDelegate?
+    
+    // 変数
+    public var sliding: Bool!
+    public var playing: Bool!
+    
+    // 部品
+    var playerLayer:AVPlayerLayer?  //player
+    public var timeLabel: UILabel!  //label
+    weak var slider: UISlider!{     // slider
+        // スライダーの値が更新されるたびに、スライダーの画面更新
+        didSet{
+            slider.setThumbImage(UIImage(systemName: "bolt.fill"), for: UIControl.State.normal)
+            slider.frame = CGRect(x: 50, y: bounds.maxY - 30, width: bounds.maxX / 2, height: 30)
+            
+            // スライダーのタップ、リリース挙動
+            slider.addTarget(self, action: #selector(sliderTouchDown), for: UIControl.Event.touchDown)
+            slider.addTarget(self, action: #selector(sliderTouchUpOut), for: UIControl.Event.touchUpOutside)
+            slider.addTarget(self, action: #selector(sliderTouchUpOut), for: UIControl.Event.touchUpInside)
+            slider.addTarget(self, action: #selector(sliderTouchUpOut), for: UIControl.Event.touchCancel)
+            
+            self.addSubview(slider)
+        }
+    }
+    var playAndPauseBtn: UIButton!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer?.frame = self.bounds
+        
+        // シークラベル
+        let labelRect = CGRect(x: bounds.maxX - 120, y: bounds.maxY - 30, width: 120, height: 30)
+        self.timeLabel = UILabel(frame: labelRect)
+        
+        // シークラベルの制御状況
+        self.sliding = false
+        self.playing = false
+        
+        // 再生・停止ボタン
+        self.playAndPauseBtn = UIButton()
+        playAndPauseBtn.frame = CGRect(x: 10, y: bounds.maxY - 30, width: 40, height: 30)
+        playAndPauseBtn.addTarget(self, action: #selector(playAndPause), for: UIControl.Event.touchUpInside)
+        
+        self.slider = UISlider()
+        
+        // 表示
+        self.addSubview(timeLabel)
+        self.addSubview(playAndPauseBtn)
+    }
+    
+    // スライダー押下
+    @objc func sliderTouchDown(slider:UISlider){
+        // 再生進捗管理？
+        self.sliding = true
+    }
+    // スライダーリリース
+    @objc func sliderTouchUpOut(slider:UISlider){
+        delegate?.testPlayer(playerView: self, sliderTouchUpOut: slider)
+    }
+    
+    @objc func playAndPause(btn: UIButton){
+        let tmp = !playing
+
+        playing = tmp
+
+        if playing {
+            playAndPauseBtn.setImage(UIImage(systemName: "stop.circle"), for: UIControl.State.normal)
+        }else{
+            playAndPauseBtn.setImage(UIImage(systemName: "play.circle"), for: UIControl.State.normal)
+        }
+
+        delegate?.testPlayer(playerView: self, playAndPause: btn)
+    }
+}
+
